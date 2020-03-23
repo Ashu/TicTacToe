b3b19f3a91c7f7f8cd99a0b6fcaf94ed204a242b
Ashutosh Dave <ashutosh.dave@eternussolutions.com>
2020-03-23 20:11:26 +0530

    Removed comments

--- TicTacToe/HomeViewController.swift
+++ TicTacToe/HomeViewController.swift
@@ -19,7 +19,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
